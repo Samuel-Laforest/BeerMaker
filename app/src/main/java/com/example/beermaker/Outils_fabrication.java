f8a68c7469d9467bc1a82eb38e98d36eaa4cd89c
Samuel-Laforest <samuel.laforest.sio@gmail.com>
2021-10-17 23:59:43 +0200

    v1

--- app/src/main/java/com/example/beermaker/Outils_fabrication.java
+++ app/src/main/java/com/example/beermaker/Outils_fabrication.java
@@ -3,23 +3,18 @@ package com.example.beermaker;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+
+import org.w3c.dom.Text;
 
 public class Outils_fabrication extends AppCompatActivity {
     @Override
     public void onPostCreate(@Nullable Bundle savedInstanceState) {
         super.onPostCreate(savedInstanceState);
         setContentView(R.layout.outils_fabrication);
-        ecouteBtnClose();
     }
 
-    private void ecouteBtnClose(){
-        findViewById(R.id.button04).setOnClickListener(new Button.OnClickListener(){
-            public void onClick(View v){
-                finish();
-            }
-        });
-    }
 }
